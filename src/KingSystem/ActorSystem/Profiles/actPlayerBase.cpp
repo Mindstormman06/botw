75fa6ce20b01f745af6ea148af4e351b1452f034
Aiden A <76793340+Mindstormman06@users.noreply.github.com>
2026-08-07 17:13:56 -0700

    act: match PlayerBase::switchEquipment

--- src/KingSystem/ActorSystem/Profiles/actPlayerBase.cpp
+++ src/KingSystem/ActorSystem/Profiles/actPlayerBase.cpp
@@ -1,1 +1,61 @@
 #include "KingSystem/ActorSystem/Profiles/actPlayerBase.h"
+#include "KingSystem/ActorSystem/actInfoData.h"
+
+namespace ksys::act {
+
+void PlayerBase::switchEquipment(const sead::SafeString& slot, int frames, int x,
+                                 const uintptr_t& y) {
+    auto lock = sead::makeScopedLock(mCS);
+
+    if (slot == "Armor_Default_Upper") {
+        mArmorUpper = slot;
+        _1168[4] = -1;
+    } else if (slot == "Armor_Default_Lower") {
+        mArmorLower = slot;
+        _1168[5] = -1;
+    } else if (slot == "Weapon_Default_Left") {
+        mWeaponLeft = slot;
+        _1168[1] = 0;
+        _1184[1] = 1;
+    } else if (slot == "Weapon_Default_Bow") {
+        mWeaponBow = slot;
+        _1168[2] = 0;
+        _1184[2] = 1;
+    } else if (slot == "Weapon_Default_Right") {
+        mWeaponRight = slot;
+        _1168[0] = 0;
+        _1184[0] = 1;
+    } else {
+        const char* profile = nullptr;
+        if (InfoData::instance()->getActorProfile(&profile, slot.cstr())) {
+            sead::SafeString prof(profile);
+            if (prof == "ArmorHead") {
+                mArrow = slot;
+                _1168[3] = x;
+            } else if (prof == "ArmorUpper") {
+                mArmorUpper = slot;
+                _1168[4] = x;
+            } else if (prof == "ArmorLower") {
+                mArmorLower = slot;
+                _1168[5] = x;
+            } else if (prof == "WeaponShield") {
+                mWeaponLeft = slot;
+                _1168[1] = frames;
+                _1184[1] = 1;
+                _119c[1] = y;
+            } else if (prof == "WeaponBow") {
+                mWeaponBow = slot;
+                _1168[2] = frames;
+                _1184[2] = 1;
+                _119c[2] = y;
+            } else {
+                mWeaponRight = slot;
+                _1168[0] = frames;
+                _1184[0] = 1;
+                _119c[0] = y;
+            }
+        }
+    }
+}
+
+}  // namespace ksys::act
--- src/KingSystem/ActorSystem/Profiles/actPlayerBase.h
+++ src/KingSystem/ActorSystem/Profiles/actPlayerBase.h
@@ -1,19 +1,33 @@
 #pragma once
 
-#include <prim/seadSafeString.h>
+#include <thread/seadCriticalSection.h>
 #include "KingSystem/ActorSystem/actActor.h"
 
 namespace ksys::act {
 
-// TODO
 class PlayerBase : public Actor {
 public:
-    // FIXME: name for x and name+type for y
     void switchEquipment(const sead::SafeString& slot, int frames, int x = -1,
                          const uintptr_t& y = {});
 
-    // FIXME: name for x
     void setExtraLife(s32 extra_life, f32 x);
+
+private:
+    u8 _840[0xeb8 - 0x840];
+    sead::CriticalSection mCS;
+    u8 _ef8[0xf00 - 0xef8];
+    sead::FixedSafeString<64> mWeaponRight;  // 0xf00
+    sead::FixedSafeString<64> mWeaponLeft;   // 0xf58
+    sead::FixedSafeString<64> mWeaponBow;    // 0xfb0
+    sead::FixedSafeString<64> mArrow;        // 0x1008
+    sead::FixedSafeString<64> mArmorUpper;   // 0x1060
+    sead::FixedSafeString<64> mArmorLower;   // 0x10b8
+    sead::FixedSafeString<64> mArmorHead;    // 0x1110
+    u32 _1168[6];
+    u32 _1180;
+    u8 _1184[12];
+    u8 _1190[12];
+    u64 _119c[3];
 };
 
 }  // namespace ksys::act
